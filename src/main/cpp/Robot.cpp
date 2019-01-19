ab9cb30ada562c2df395262ba36abcf1def834d0
Bryce Priebe <priebebryce@gmail.com>
2019-01-19 15:17:21 -0500

    Added vision code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -10,11 +10,13 @@
 #include <iostream>
 
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <cameraserver/cameraserver.h>
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  frc::CameraServer::GetInstance()->StartAutomaticCapture();
 }
 
 /**
